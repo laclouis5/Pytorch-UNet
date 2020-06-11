eb44edbfda2ba44be50a66f11824b1689a6b77c8
Louis Lac <lac.louis5@gmail.com>
2020-06-11 11:18:23 +0200

    parser

--- train.py
+++ train.py
@@ -27,8 +27,8 @@ def train_net(net,
     batch_size=1,
     lr=0.001,
     val_percent=0.1,
-    save_cp=True,
-    img_scale=0.5
+    save_cp=False,
+    img_size=None
 ):
     # dataset = BasicDataset(dir_img, dir_mask, img_scale)
     # n_val = int(len(dataset) * val_percent)
@@ -36,16 +36,16 @@ def train_net(net,
     # (train, val) = random_split(dataset, [n_train, n_val])
     # train.transform = UNetDataAugmentations(img_scale)
     train = CustomDataset("data/train/images/", "data/train/masks/",
-        transform=UNetDataAugmentations(True))
+        transform=UNetDataAugmentations(img_size))
     val = CustomDataset("data/val/images/", "data/val/masks/",
-        transform=UNetBaseTransform(True))
+        transform=UNetBaseTransform(img_size))
 
     train_loader = DataLoader(train,
         batch_size=batch_size, shuffle=True, num_workers=8, pin_memory=True)
     val_loader = DataLoader(val,
         batch_size=1, shuffle=False, num_workers=8, pin_memory=True)
 
-    writer = SummaryWriter(comment=f"LR_{lr}_BS_{batch_size}_SCALE_{img_scale}")
+    writer = SummaryWriter(comment=f"LR_{lr}_BS_{batch_size}_SIZE_{img_size}")
     global_step = 0
 
     logging.info(
@@ -57,7 +57,7 @@ def train_net(net,
         f"\tValidation size: {len(val)}\n"
         f"\tCheckpoints:     {save_cp}\n"
         f"\tDevice:          {device.type}\n"
-        f"\tImages scaling:  {img_scale}")
+        f"\tImages size:     {img_size}")
 
     criterion = nn.CrossEntropyLoss() if net.n_classes > 1 else nn.BCEWithLogitsLoss()
     optimizer = optim.RMSprop(net.parameters(), lr=lr, weight_decay=1e-8, momentum=0.9)
@@ -100,20 +100,13 @@ def train_net(net,
 
                 global_step += 1
                 if global_step % (len(train) // (5 * batch_size)) == 0:
-                    # for tag, value in net.named_parameters():
-                        # tag = tag.replace(".", "/")
-
-                        # writer.add_histogram("weights/" + tag, value.data.cpu().numpy(), global_step)
-                        # writer.add_histogram("grads/" + tag, value.grad.data.cpu().numpy(), global_step)
-
                     val_score = eval_net(net, val_loader, device)
-                    # scheduler.step(val_score)
+                    # scheduler.step(val_score)  # PlateauScheduler
 
                     if val_score > best_model_dice_coeff:
                         best_model_dice_coeff = val_score
                         torch.save(net.state_dict(),
                             dir_checkpoint + f'CP_best.pth')
-                        print("Best network saved.")
 
                     writer.add_scalar("learning_rate",
                         optimizer.param_groups[0]["lr"],
@@ -129,7 +122,9 @@ def train_net(net,
                     writer.add_images("images", imgs, global_step)
 
                     if net.n_classes == 1:
-                        writer.add_images("masks/true", true_masks, global_step)
+                        writer.add_images("masks/true",
+                            true_masks,
+                            global_step)
                         writer.add_images("masks/pred",
                             torch.sigmoid(masks_pred) > 0.5,
                             global_step)
@@ -151,7 +146,8 @@ def train_net(net,
 
 def get_args():
     parser = argparse.ArgumentParser(
-        description="Train the UNet on images and target masks",         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        description="Train the UNet on images and target masks",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-e", "--epochs",
         metavar="E", type=int, default=5, help="Number of epochs", dest="epochs")
     parser.add_argument("-b", "--batch-size",
@@ -160,8 +156,12 @@ def get_args():
         metavar="LR", type=float, nargs="?", default=0.01, help="Learning rate", dest="lr")
     parser.add_argument("-f", "--load",
         dest="load", type=str, default=False, help="Load model from a .pth file")
-    parser.add_argument("-s", "--scale",
-        dest="scale", type=float, default=0.5, help="Downscaling factor of the images")
+    # parser.add_argument("-s", "--scale",
+    #     dest="scale", type=float, default=0.5, help="Downscaling factor of the images")
+    parser.add_argument("--width",
+        dest="width", type=int, help="Resize width for input image.")
+    parser.add_argument("--height",
+        dest="height", type=int, help="Resize height for input image.")
     parser.add_argument("-v", "--validation",
         dest="val", type=float, default=10.0,
         help="Percent of the data that is used as validation (0-100)")
@@ -181,7 +181,7 @@ if __name__ == "__main__":
     #   - For 2 classes, use n_classes=1
     #   - For N > 2 classes, use n_classes=N
     # net = UNet(n_channels=3, n_classes=1, bilinear=True)
-    net = smp.FPN("resnet18")
+    net = smp.Unet("resnet34")
     setattr(net, "n_classes", 1)
     setattr(net, "n_channels", 3)
     setattr(net, "bilinear", None)
@@ -206,7 +206,7 @@ if __name__ == "__main__":
             batch_size=args.batchsize,
             lr=args.lr,
             device=device,
-            img_scale=args.scale,
+            img_size=(args.height, args.width),
             val_percent=args.val / 100)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), "INTERRUPTED.pth")
